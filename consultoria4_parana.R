da3a02d61a1e5a7977a9ede26b187ba7232050ab
JoaoPedroFM <jpfmartins@id.uff.br>
2022-01-30 12:36:48 -0300

    shapes e codigo

--- consultoria4_parana.R
+++ consultoria4_parana.R
@@ -2,25 +2,39 @@ library(tidyverse)
 library(dplyr)
 library(readxl)
 library(epiDisplay)
+{library(CARBayesdata)
+  library(sp)
+  library(dplyr)
+  library(sf)
+  library(ggplot2)
+  library(spdep)
+  library(spatialreg)
+  library(rgdal)
+  library(leaflet)}
 
 base = read_csv2("suicidios.csv")
 municipio = read_excel("Municipios_2010.xlsx")
-parana = base |> 
+sul = base |> 
   mutate(UF= str_sub(Microrregiao, 1, 2)) |> 
-  filter(UF== 41)
-parana$Microrregiao = as.character(parana$Microrregiao)
+  filter(UF== 41|UF== 42|UF== 43)
+sul$Microrregiao = as.character(sul$Microrregiao)
+
+# fazendo o mapa da proporcao
+prop = sul |> 
+  dplyr::select(c(1,2)) |> 
+  group_by(Microrregiao) |> 
+  summarise(media = mean(suicidio_paf))
 
 library(sf)
 PR = st_read("PR_Microrregioes_2020.shp")
-# juntando as informacoes numa base unica
-parana <- left_join(x=PR, y=parana, by=c("CD_MICRO"="Microrregiao"))
-head(parana)
+SC = st_read("SC_Microrregioes_2020.shp")
+RS = st_read("RS_Microrregioes_2020.shp")
 
-# fazendo o mapa da proporcao
-prop = parana |> 
-  dplyr::select(c(1:4,13)) |> 
-  group_by(NM_MICRO) |> 
-  summarise(media = mean(suicidio_paf))
+SUL_SF = rbind(PR, SC, RS)
+
+# juntando as informacoes numa base unica
+prop <- left_join(x=SUL_SF, y=prop, by=c("CD_MICRO"="Microrregiao"))
+head(prop)
 
 colours <- colorNumeric(palette = "YlOrRd", domain = prop$media)
 map1 <- leaflet(data=prop) |>
@@ -31,5 +45,87 @@ map1 <- leaflet(data=prop) |>
             title="media") |>
   addScaleBar(position="bottomleft")
 map1
-# mais escuros, maiores os preços
-# localmente há áreas que formam clusters em relação aos preços
+# mais escuros, maiores os precos
+# localmente ha areas que formam clusters em relacao aos precos
+
+#Tratando a sul de dados
+sul = na.omit(sul) |>  # 2 observacoes com valores faltantes
+  mutate(suicidio_paf = factor(suicidio_paf, labels = c("Nao","Sim")),
+         id_legal = factor(id_legal, labels = c("Nao","Sim")),
+         trab_armado = factor(trab_armado, labels = c("Nao","Sim")),
+         sexo = factor(sexo, labels = c("Masculino","Feminino")),
+         raca = factor(raca, labels = c("Branca","Preta","Amarela",
+                                        "Parda","Indigena","Ignorado")),
+         estado_civil = factor(estado_civil,
+                             labels = c("Solteiro","Casado","Viuvo",
+                                        "Separado","Divorciado","Ignorado")),
+         escolaridade = factor(escolaridade,
+                               labels = c("Nenhuma","1 a 3 anos","4 a 7 anos",
+                                          "8 a 11 anos","12 anos ou mais","Ignorado")))
+
+################################################################################
+# ANALISE EXPLORATORIA
+
+# Tabela de frequencias
+tab1(sul$suicidio_paf,graph = F)
+tab1(sul$id_legal,graph = F)
+tab1(sul$trab_armado,graph = F)
+tab1(sul$sexo,graph = F)
+tab1(sul$raca,graph = F)
+tab1(sul$estado_civil,graph = F)
+tab1(sul$escolaridade,graph = F)
+
+
+# IDADE
+#Histograma da idade dos candidatos (população geral)
+sul |> 
+  ggplot(mapping = aes(x = idade)) +
+  geom_histogram(fill= "darkred", col="white")+
+  # scale_x_continuous(breaks = seq(15,90,5))+
+  scale_y_continuous(label = scales::label_number(big.mark = ".",
+                                                  decimal.mark = ",")) +
+  labs(y= "Frequencia", x="Idade") + theme_classic() + 
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5, 
+                                                            size=14, face="bold"), 
+        text = element_text(size=15), plot.subtitle = element_text(hjust = 0.5, size=12))
+
+
+################################################################################
+# MODELAGEM
+
+#Carregando pacote
+library(rstanarm)
+
+#Ajustando o modelo
+ajuste = stan_glm(formula = suicidio_paf ~ idade + id_legal + trab_armado + sexo + 
+                    raca + estado_civil + escolaridade,
+                  data = sul,
+                  family = binomial(link = "logit"),
+                  prior_intercept = normal(0,10),
+                  refresh = 0,
+                  chain = 2,
+                  iter = 1000,
+                  warmup = 200,
+                  thin = 4)
+
+#Visualizando o ajuste
+ajuste$stanfit
+
+save(ajuste, file="ajuste.rdata")
+load("ajuste.rdata")
+
+#Plotando os intervalos de credibilidade
+plot(ajuste, prob = 0.9)
+
+#Calculando intervalo de credibilidade
+posterior_interval(object = ajuste, 
+                   regex_pars = "id_legal", 
+                   prob = 0.8)
+
+#Visualizando convergencia
+plot(ajuste, 
+     plotfun = "combo", 
+     regex_pars = "id_legal")
+
+#Entendo as prioris utilizadas
+prior_summary(ajuste)
